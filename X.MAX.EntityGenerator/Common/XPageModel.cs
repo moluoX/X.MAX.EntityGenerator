0ea14a2e9b93eef5fccb3f34a5a26b75caf091e0
X.MAX <minaxu04@126.com>
2018-11-29 12:23:50 +0800

    page

---
+++ X.MAX.EntityGenerator/Common/XPageModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace X.MAX.EntityGenerator.Common
+{
+    public class XPageModel : PageModel
+    {
+        public virtual ContentResult XJson(object m)
+        {
+            return Content(JsonConvert.SerializeObject(m), "application/json");
+        }
+    }
+}
--- X.MAX.EntityGenerator/Pages/Index.cshtml.cs
+++ X.MAX.EntityGenerator/Pages/Index.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using X.MAX.EntityGenerator.Common;
 
 namespace X.MAX.EntityGenerator.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel : XPageModel
     {
-        public void OnGet()
-        {
+        [TempData]
+        public string S { get; set; }
 
+        public IList<string> Tables { get; set; }
+
+        public void OnGet(string s)
+        {
+            S = s;
+            Tables = new[] { "1", "2" };
+        }
+
+        public IActionResult OnPost(IList<string> tables)
+        {
+            return XJson(new { code = 0, data = @"E:\code\ycloud.task\4_Manager\YCLOUD.Start\bin\Debug" });
         }
     }
 }
